7b9a68119e17d95bc179717839bb7d597596b664
GRWang <frantik@qq.com>
2019-01-25 23:27:19 +0800

    Update office index page.

--- office/urls.py
+++ office/urls.py
@@ -10,13 +10,15 @@
 @Description:
 """
 from django.urls import path, include
-from .views import OfficeView, DocumentDetailView, DocumentListView, UserManageView
 from django.contrib.auth.decorators import login_required
+from .views import OfficeView, DocumentDetailView, DocumentListView, UserManageView, UserMessageView, AddressBookView
 
 app_name = 'office'
 urlpatterns = [
-    path('', login_required()(OfficeView.as_view()), name='index'),
-    path('document/', login_required()(DocumentListView.as_view()), name='document'),
-    path('document/<int:number>', login_required()(DocumentDetailView.as_view()), name='document_detail'),
-    path('usermanagement/', login_required()(UserManageView.as_view()), name='user_manage')
+    path('', login_required()(OfficeView.as_view()), name="index"),
+    path('document/', login_required()(DocumentListView.as_view()), name="document"),
+    path('document/<int:number>', login_required()(DocumentDetailView.as_view()), name="document_detail"),
+    path('usermanagement/', login_required()(UserManageView.as_view()), name="user_manage"),
+    path('message/', login_required()(UserMessageView.as_view()), name="user_message"),
+    path('addressbook/', login_required()(AddressBookView.as_view()), name="address_book"),
 ]
